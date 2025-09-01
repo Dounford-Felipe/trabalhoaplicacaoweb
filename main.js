f39fba44df542791255a1485d8781e6266fd8aab
Dounford-Felipe <92835771+Dounford-Felipe@users.noreply.github.com>
2025-09-01 20:14:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let currentTab = "formacao";
+function changeTab(page) {
+    /*Decidi por ter uma variável com a aba atual e só alterar ela, para facilitar a alteração do botão também
+    Uma outra opção seria document.getElementById("div_id").style.display = "none"; para cada div
+    Uma terceira opção seria usar querySelectorAll("main div"), aqui pode ser exagero já que são apenas 4 divs, mas evita repetição de código*/
+    document.getElementById(currentTab).style.display = "none";
+    document.getElementById(currentTab + "Btn").classList.remove("active");
+    
+    currentTab = page;
+    document.getElementById(page).style.display = "";
+    document.getElementById(page + "Btn").classList.add("active");
+}
